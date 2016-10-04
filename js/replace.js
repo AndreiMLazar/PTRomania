8f283ace444a8c6f4ab93b2b30df85761dc8be20
AndreiMLazar <l.andrei37@yahoo.ro>
2016-10-04 23:01:35 +0300

    Site Created
    Added the work I've done before GitHub

---
+++ js/replace.js
@@ -0,0 +1,3 @@
+var newHtml = document.getElementById('replace') ;
+newHtml.innerHTML = newHtml.innerHTML
+                        .replace('<div class="col-offset-1"></div><div class="col-11"><div class="title" alt="Pagina Despre">Despre</div></div>','');
