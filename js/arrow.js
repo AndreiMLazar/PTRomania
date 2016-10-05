77ce654b592b84a816a033e389631648e3591477
AndreiMLazar <l.andrei37@yahoo.ro>
2016-10-05 16:25:55 +0300

    JS Arrow fix

--- js/arrow.js
+++ js/arrow.js
@@ -1,42 +1,16 @@
-//this is where we apply opacity to the arrow
-$(window).scroll( function(){
-
-  //get scroll position
-  var topWindow = $(window).scrollTop();
-  //multipl by 1.5 so the arrow will become transparent half-way up the page
-  var topWindow = topWindow * 2;
-  
-  //get height of window
-  var windowHeight = $(window).height();
-      
-  //set position as percentage of how far the user has scrolled 
-  var position = topWindow / windowHeight;
-  //invert the percentage
-  position = 1 - position;
-
-  //define arrow opacity as based on how far up the page the user has scrolled
-  //no scrolling = 1, half-way up the page = 0
-  $('.arrow-wrap').css('opacity', position);
-
+jQuery.fn.extend({
+  scrollTo : function(speed, easing) {
+    return this.each(function() {
+      var targetOffset = $(this).offset().top;
+      $('html,body').animate({scrollTop: targetOffset}, speed, easing);
+    });
+  }
 });
 
-
-
-
-
-
-//Code stolen from css-tricks for smooth scrolling:
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1200);
-        return false;
-      }
-    }
-  });
+$('.arrow-wrap').click(function(e){
+    e.preventDefault();
+    var $this = $(this),
+        $next = $this.parent().next();
+    
+    $next.scrollTo(800, 'swing');
 });
